726cf487c133fbb90638142ed562ef6c0b75b5e8
AndyFlave <35171083+AndyFlave@users.noreply.github.com>
2021-01-21 23:46:18 +0300

    clean sidebar function

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+const $vidgetAdd = document.querySelector('.vidget-add')
+const $sidebar = document.querySelector('.sidebar')
+
+
+$vidgetAdd.addEventListener('click', e => {
+	$sidebar.innerHTML = ''
+})
